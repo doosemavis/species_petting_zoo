fe939ddb49aff688d04fc30669531199e946c668
doosemavis <moosedavis2011@gmail.com>
2021-01-07 14:45:28 -0600

    add species button and form

--- spz-front-end/category.js
+++ spz-front-end/category.js
@@ -9,14 +9,55 @@ class Category {
         let main = document.getElementById('main')
         main.innerHTML = `
         <h3> Genus: ${this.name}</h3>
+        <button id="animal-form" data-id="${this.id}">Add Species</button>
         <hr>
         <br>
         `
+        
         main.innerHTML += this.animals.map(animal => 
             `
             <h4> Species: ${animal.name}</h4>
-            <li>Laymans Name: ${animal.layman}</li>
+            <ul>Laymans Name: ${animal.layman}</ul>
+            <ul>Age: ${animal.age}</ul>
+            <ul>Extinct: ${animal.extinct}</ul>
+            <br>
             ` 
         ).join("") 
     }
 }
+
+    // displayCreateAnimalForm() {
+    //     let formDiv = document.querySelector("#new-animal-form")
+    //     let html = `
+    //         <form>
+    //             <label>Species:</label>
+    //             <input type="text" id="animal">
+    //             <input type="text" id="layman">
+    //             <input type="text" id="age">
+    //             <input type="text" id="extinct">
+    //             <input type="submit">
+    //         </form>
+    //     `
+    //     formDiv.innerHTML = html
+    //     document.querySelector('form').addEventListener('submit', createAnimal)
+    // }
+
+    // attachClicksToBtn() {
+    //     let animals = document.querySelectorAll("li a")
+    //     animals.forEach(animal => {
+    //         animal.addEventListener('click', )
+    //     })
+    // }
+
+
+    //     displayAnimal(e) {
+    //         let id = e.target.dataset.id
+    //         let main = document.getElementById('main')
+    //         main.innerHTML += ""
+    //         fetch(BASE_URL + `/categories/${id}`)
+    //         .then(resp => resp.json())
+    //         .then(data => {
+    //             let animal = new Animal(data)
+    //             animal.renderCategory()
+    //         })
+    //     }
--- spz-front-end/index.js
+++ spz-front-end/index.js
@@ -25,6 +25,7 @@ function clearForm() {
     formDiv.innerHTML = ""
 }
 
+
 function createCategory(e) {
     e.preventDefault()
     let main = document.getElementById('main')
@@ -56,8 +57,6 @@ function createCategory(e) {
 function getCategories() {
     let main = document.getElementById('main')
     main.innerHTML = ''
-    // fetch(BASE_URL + '/categories')
-    // .then(res => res.json())
     fetchCategories()
     .then(categories => {categories.map(category => {
         main.innerHTML += `
@@ -92,5 +91,91 @@ function displayCategory(e) {
     .then(data => {
         let category = new Category(data)
         category.renderCategory()
+        document.getElementById('animal-form').addEventListener('click', displayCreateAnimalForm)
     })
 }
+
+
+function displayCreateAnimalForm() {
+    let formDiv = document.querySelector("#new-animal-form")
+    let html = `
+        <form>
+            <label>Species:</label>
+            <input type="text" id="animal">
+            <br>
+            <label>Laymans:</label>
+            <input type="text" id="layman">
+            <br>
+            <label>Age:</label>
+            <input type="text" id="age">
+            <br>
+            <label>Extinct:</label>
+            <input type="text" id="extinct">
+            <br>
+            <input type="submit">
+        </form>
+    `
+    formDiv.innerHTML = html
+    document.querySelector('form').addEventListener('submit', createAnimal)
+}
+
+function attachClicksToBtn() {
+    let animals = document.querySelectorAll("li a")
+    animals.forEach(animal => {
+        animal.addEventListener('click', )
+    })
+}
+
+
+function createAnimal(e) {
+    e.preventDefault()
+    let main = document.getElementById('main')
+    let animal = {
+        name: e.target.querySelector("#animal").value
+    }
+    let configObj = {
+        method: 'POST',
+        body: JSON.stringify(animal),
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    }
+    fetch(BASE_URL + '/categories', configObj)
+    .then(res => res.json())
+    .then(animal => {
+        main.innerHTML += `
+        <li>
+        <a href="#" data-id="${animal.id}">${animal.name}</a>
+        </li>
+        `
+        attachClicksToBtn()
+        // clearForm()
+        }
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+// function displayCreateAnimalForm() {
+//     let formDiv = document.querySelector("#new-animal-form")
+//     let html = `
+//         <form>
+//             <label>Species:</label>
+//             <input type="text" id="animal">
+//             <input type="submit">
+//         </form>
+//     `
+//     formDiv.innerHTML = html
+//     document.querySelector('form').addEventListener('submit', createAnimal)
+// }
+
+// document.getElementById('animal-form').addEventListener('click', displayCreateAnimalForm)
